2f671e165d7c55e12470d05027b4bb514595d1d5
modestasgestas <modestas@kiro.tech>
2020-01-05 15:21:02 +0200

    Messages functionality

--- app/Message.php
+++ app/Message.php
@@ -25,70 +25,80 @@ class Message extends Model
     {
         $msg_sender =  Message::where(['sender_id' => $request['sender_id']])->where(['receiver_id' => $request['receiver_id']])->get();
         $msg_receiver =  Message::where(['sender_id' => $request['receiver_id']])->where(['receiver_id' => $request['sender_id']])->get();
-        $return  = [
 
-        ];
+        $sender_msgs = $this->GetOneCatMessages($msg_sender);
+        $receiver_msgs = $this->GetOneCatMessages($msg_receiver);
+        $return_array = $this->mergeAndSortArrays($sender_msgs,$receiver_msgs);
+        $array = collect($return_array)->sortByDesc('created_at')->take(9)->reverse();
+        return $array;
+    }
+
+    public function GetOneCatMessages($msg_sender){
+        $return = [];
+        $user = new User();
         $i = 0;
         foreach($msg_sender as $msg){
+            $user_ob = $user->GetUser($msg["sender_id"]);
+            $msg["sender_username"] = $user_ob->username;
             $return[$i] = $msg;
             $i++;
         }
-        foreach($msg_receiver as $msg){
-            $return[$i] = $msg;
-            $i++;
-        }
-
         return $return;
     }
 
     public function GetUserMessagesContacts($user_id){
         $messages_sender = Message::where(['sender_id' =>$user_id])->get();
         $messages_receiver = Message::where(['receiver_id'=> $user_id])->get();
-        $returnData = [];
+        $sender_array = $this->GetSenders($messages_sender);
+        $receiver_array = $this->GetSenders($messages_receiver);
+
+        return $this->mergeAndSortArrays($sender_array,$receiver_array);
+    }
+
+    public function GetSenders($messages_sender){
         $i = 0;
         $ids=[];
         $user= new User();
+        $returnData = [];
         foreach($messages_sender as $message){
+            $sender= false;
+            $user_id = '';
+            $received = false;
             if(!in_array($message["sender_id"],$ids) && $message["sender_id"] != $user_id){
-                $ids[$i] = $message["sender_id"];
-                $user_ob = $user->GetUser($message["sender_id"]);
-                $returnData[$i] = [
-                    "id" => $message["sender_id"],
-                    "user_name" => $user_ob->username
-                ];
-                $i++;
+                $sender = true;
+                $user_id = $message["sender_id"];
             }
             if(!in_array($message["receiver_id"],$ids) && $message["receiver_id"] != $user_id){
-                $ids[$i] = $message["receiver_id"];
-                $user_ob = $user->GetUser($message["sender_id"]);
-                $returnData[$i] = [
-                    "id" => $message["receiver_id"],
-                    "user_name" => $user_ob->username
-                ];
-                $i++;
+                $received = true;
+                $user_id =$message["receiver_id"];
             }
-        }
 
-        foreach($messages_receiver as $message){
-            if(!in_array($message["sender_id"],$ids) && $message["sender_id"] != $user_id){
-                $ids[$i] = $message["sender_id"];
-                $user_ob = $user->GetUser($message["sender_id"]);
+            if($sender  && $received ){
+                $ids[$i] = $user_id;
+                $user_ob = $user->GetUser($user_id);
                 $returnData[$i] = [
-                    "id" => $message["sender_id"],
-                    "user_name" => $user_ob->username
-                ];
-                $i++;
-            }
-            if(!in_array($message["receiver_id"],$ids)&& $message["receiver_id"] != $user_id){
-                $ids[$i] = $message["receiver_id"];
-                $user_ob = $user->GetUser($message["sender_id"]);
-                $returnData[$i] = [
-                    "id" => $message["receiver_id"],
+                    "id" => $user_id,
                     "user_name" => $user_ob->username
                 ];
                 $i++;
             }
         }
         return $returnData;
+    }
+
+    public function mergeAndSortArrays($firstArray,$secondArray){
+        $array_to_return = [];
+        $i = 0;
+        foreach($firstArray as $msg){
+            $array_to_return[$i] = $msg;
+            $i++;
+        }
+
+        foreach($secondArray as $msg){
+            $array_to_return[$i] = $msg;
+            $i++;
+        }
+
+        return $array_to_return;
     }
 }
